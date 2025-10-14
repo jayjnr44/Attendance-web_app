72af06aac58c96c04457e962d54c15e0da6a2c0a
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-14 23:54:22 +0000

    configured  restframework configured jwt settings configured media files for export reports

--- attendance_webapp/settings.py
+++ attendance_webapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
     'users',
+    'attendance',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CONFIGURING REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
+
+#CONFIGURING JWT SETTINGS
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),    
+}
+
+#SETTING CUSTOM USERS
+AUTH_USER_MODEL = 'users.CustomUser'
+
+#CONFIGURE MEDIA FILES FOR REPORT EXPORTS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
