c09b7be15255876f13a584fedcfce982c822c742
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-18 01:02:08 +0000

    admin configuration for user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'is_active']
+    list_filter = ['role', 'is_active', 'date_joined' ]
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+
+    fieldsets = UserAdmin.fieldsets + (
+
+        ('Additional Info', {'fields': ('role', 'phone')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('role','email' , 'phone')}),
+    )
