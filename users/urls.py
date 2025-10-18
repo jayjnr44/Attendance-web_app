fe83b3bc5ba44039258319ef6b4c3aa2af2fb06e
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-18 18:22:31 +0000

    configured routes for users app urls

---
+++ users/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    UserRegistrationView,
+    UserLoginView,
+    UserProfileView,
+    ChangePasswordView,
+    UserListView,
+)
+
+app_name = 'users'
+
+urlpatterns = [
+    # Authentication
+    path('auth/register/', UserRegistrationView.as_view(), name='register'),
+    path('auth/login/', UserLoginView.as_view(), name='login'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/profile/', UserProfileView.as_view(), name='profile'),
+    path('auth/change-password/', ChangePasswordView.as_view(), name='change_password'),
+    
+    # User management
+    path('users/', UserListView.as_view(), name='user_list'),
+]
