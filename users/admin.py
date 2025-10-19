84db1c0022522c7f029a3325922599e0fd31a76c
Felix Kuffour <Jayycrypt@gmail.com>
2025-10-19 21:37:43 +0000

    users/admin.py date joined made reread only field

--- users/admin.py
+++ users/admin.py
@@ -8,7 +8,8 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'is_active']
     list_filter = ['role', 'is_active', 'date_joined' ]
     search_fields = ['username', 'email', 'first_name', 'last_name']
-
+    readonly_fields = ['date_joined']
+    
     fieldsets = UserAdmin.fieldsets + (
 
         ('Additional Info', {'fields': ('role', 'phone')}),
